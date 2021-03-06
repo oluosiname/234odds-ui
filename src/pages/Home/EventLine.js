af0a30a016af2514c07772ca8130ca63e4fa0e3a
Olumuyiwa Osiname <olumuyiwa.osiname@homeday.de>
2021-03-06 19:40:27 +0100

    fix padding

--- src/pages/Home/EventLine.js
+++ src/pages/Home/EventLine.js
@@ -7,7 +7,7 @@ import parameterize from "parameterize";
 const EventLine = ({ event }) => {
   return (
     <Link to={`/events/${event.uid}`}>
-      <div className="flex bg-brand-primary-100 p-1.5 px-5 border-b border-brand-primary-200 ">
+      <div className="flex bg-brand-primary-100 p-2 px-5 border-b border-brand-primary-200 ">
         <div className="w-3/5 flex text-gray-100 text-xs">
           <div className="time w-1/4">{event.kickoff}</div>
           <div className="teams flex flex-col  w-3/4 justify-between">
@@ -32,13 +32,13 @@ const EventLine = ({ event }) => {
           </div>
         </div>
         <div className="w-2/5 text-xs text-gray-100 space-x-2 flex text-white justify-end">
-          <span className="bg-gray-300 flex rounded-md px-2 py-2">
+          <span className="bg-gray-300 flex rounded-md px-2 py-2.5">
             {event.top_odds.home}
           </span>
-          <span className="bg-gray-300 flex rounded-md px-2 py-2">
+          <span className="bg-gray-300 flex rounded-md px-2 py-2.5">
             {event.top_odds.draw}
           </span>
-          <span className="bg-gray-300 flex rounded-md px-2 py-2">
+          <span className="bg-gray-300 flex rounded-md px-2 py-2.5">
             {event.top_odds.away}
           </span>
         </div>
