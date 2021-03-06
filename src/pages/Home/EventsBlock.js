e5c2b520b0eb2ada2c1c89ad064a367f1c2a6161
Olumuyiwa Osiname <olumuyiwa.osiname@homeday.de>
2021-03-06 11:28:16 +0100

    group events according to day

--- src/pages/Home/EventsBlock.js
+++ src/pages/Home/EventsBlock.js
@@ -11,7 +11,6 @@ const EventsBlock = ({ data }) => {
         return (
           <div key={competition}>
             <div className=" bg-gray-200 text-gray-100 uppercase text-xs align-middle p-2 px-5 flex items-center space-x-4">
-              {/* <Icon name="icon-england" className="w-4" /> */}
               <Icon
                 name={`icon-${parameterize(competition)}`}
                 className="w-4"
@@ -19,16 +18,24 @@ const EventsBlock = ({ data }) => {
               />
               <span className="uppercase">{competition}</span>
             </div>
-            <div className="flex bg-gray-300 p-1 px-5">
-              <div className="w-3/5 text-xs text-gray-100">Today</div>
-              <div className="w-2/5 text-xs text-gray-100 space-x-3 flex justify-end">
-                <span className="px-1">Home</span>
-                <span className=" px-1">Draw</span>
-                <span className="px-1">Away</span>
-              </div>
-            </div>
-            {events.map((event) => {
-              return <EventLine event={event} key={event.uid.toString()} />;
+            {events.map(({ day, day_events }) => {
+              return (
+                <div key={day}>
+                  <div className="flex bg-gray-300 p-1 px-5">
+                    <div className="w-3/5 text-xs text-gray-100">{day}</div>
+                    <div className="w-2/5 text-xs text-gray-100 space-x-3 flex justify-end">
+                      <span className="px-1">Home</span>
+                      <span className=" px-1">Draw</span>
+                      <span className="px-1">Away</span>
+                    </div>
+                  </div>
+                  {day_events.map((event) => {
+                    return (
+                      <EventLine event={event} key={event.uid.toString()} />
+                    );
+                  })}
+                </div>
+              );
             })}
           </div>
         );
