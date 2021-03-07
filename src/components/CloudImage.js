43c56bc5ac78a63c0d2fc918a9b8e18307799b41
Olumuyiwa Osiname <olumuyiwa.osiname@homeday.de>
2021-03-07 14:47:25 +0100

    refactor

--- src/components/CloudImage.js
+++ src/components/CloudImage.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Image } from "cloudinary-react";
 
-const CloudImage = ({ name, className }) => {
+const CloudImage = ({ name, className, width, height }) => {
   return (
     <Image
       cloudName="uniodds"
       publicId={`icon-${name}.png`}
       className={className}
       quality="auto"
-      width="32"
-      height="36"
+      width={width}
       crop="scale"
     />
   );
+};
+
+CloudImage.defaultProps = {
+  width: "32",
 };
 
 export default CloudImage;
