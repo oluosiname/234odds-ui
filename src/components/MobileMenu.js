48edcca78e112ea68dc48af5bc99fd7881738fed
Olumuyiwa <oluosiname@gmail.com>
2021-03-27 13:01:06 +0100

    fix padding

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -4,9 +4,9 @@ import Icon from "./Icon/Icon";
 
 const MobileMenu = () => {
   return (
-    <div className="flex justify-between py-2.5 px-5 md:hidden bg-brand-primary-100 text-gray-100">
+    <div className="flex justify-between py-2.5 px-1 md:hidden bg-brand-primary-100 text-gray-100">
       <Link to="/">234Odds</Link>
-      <Icon name="icon-menu" />
+      <Icon name="icon-menu" size="2rem" />
     </div>
   );
 };
